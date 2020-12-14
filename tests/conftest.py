4f5dcc53103928b5246b2e3a705fcd2ce794f826
Stephen Polcyn <spolcyn@users.noreply.github.com>
2020-12-14 16:38:34 -0500

    Refactor BIDS-I fixtures to central fixture file

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,19 @@
 # Note: Could modularize this further by creating a fixtures dir and importing
 # See: https://gist.github.com/peterhurford/09f7dcda0ab04b95c026c60fa49c2a68
 import os
+import tempfile
 
 import pydicom
 import pytest
 
 from common import test_dicomFilePath
-from rtCommon.imageHandling import readDicomFromFile
+from rtCommon.imageHandling import (
+    readDicomFromFile,
+    convertDicomFileToNifti,
+    readNifti
+)
+from rtCommon.bidsIncremental import BidsIncremental
+import rtCommon.bidsLibrary as bl
 
 """ BEGIN DICOM RELATED FIXTURES """
 
@@ -36,8 +43,49 @@ def dicomImage(dicomMetadataSample) -> pydicom.dataset.Dataset:
 
     return dicom
 
+# Public metadata for test DICOM file
+@pytest.fixture
+def dicomImageMetadata(dicomImage):
+    public, _ = bl.getMetadata(dicomImage)
+    return public
+
 """ END DICOM RELATED FIXTURES """
 
 """ BEGIN BIDS RELATED FIXTURES """
+
+@pytest.yield_fixture
+def sampleNiftiImage():
+    dicomPath = os.path.join(os.path.dirname(__file__), test_dicomFilePath)
+    niftiPath = os.path.join(tempfile.gettempdir(), "test.nii")
+    convertDicomFileToNifti(dicomPath, niftiPath)
+
+    # 'Yield' will return the Nifti image now, and delay the running of
+    # subsequent code (i.e., the removal of the Nifti image on disk) until the
+    # end-of-test tear-down is being done
+    # This ensures the test doesn't create clutter, but is able to access the
+    # necessary data during the test
+    yield readNifti(niftiPath)
+
+    os.remove(niftiPath)
+
+@pytest.fixture
+def imageMetadataDict():
+    """
+    Dictionary with all required metadata to construct a BIDS-Incremental, as
+    well as extra metadata extracted from the test DICOM image.
+    """
+    return {'subject': '01', 'task': 'story', 'suffix': 'bold',  # REQUIRED
+            'session': '01', 'run': '01'}  # EXTRA
+
+@pytest.fixture
+def validBidsI(sampleNiftiImage, imageMetadataDict, dicomImageMetadata):
+    """
+    Constructs and returns a known-valid BIDS-Incremental using known metadata.
+    """
+    return BidsIncremental(image=sampleNiftiImage,
+                           subject=imageMetadataDict["subject"],
+                           task=imageMetadataDict["task"],
+                           suffix=imageMetadataDict["suffix"],
+                           imgMetadata=dicomImageMetadata)
 
 """ END BIDS RELATED FIXTURES """
--- tests/test_bidsIncremental.py
+++ tests/test_bidsIncremental.py
@@ -4,7 +4,6 @@ import os
 import pickle
 import shutil
 import tempfile
-import time
 
 from bids_validator import BIDSValidator
 import pytest
@@ -13,58 +12,23 @@ import numpy as np
 from rtCommon.bidsIncremental import BidsIncremental
 from rtCommon.bidsCommon import BidsFileExtension as BidsFileExtension
 from rtCommon.errors import ValidationError
-from rtCommon.imageHandling import convertDicomFileToNifti, readNifti
-from tests.common import test_inputDir, test_dicomFile
 
 logger = logging.getLogger(__name__)
 
-@pytest.yield_fixture
-def sampleNiftiImage():
-    dicomPath = os.path.join(os.path.dirname(__file__),
-                             test_inputDir,
-                             test_dicomFile)
-    niftiPath = os.path.join(tempfile.gettempdir(), "test.nii")
-    convertDicomFileToNifti(dicomPath, niftiPath)
-
-    # 'Yield' will return the Nifti image now, and delay the running of
-    # subsequent code (i.e., the removal of the Nifti image on disk) until the
-    # end-of-test tear-down is being done
-    # This ensures the test doesn't create clutter, but is able to access the
-    # necessary data during the test
-    yield readNifti(niftiPath)
-
-    os.remove(niftiPath)
-
-@pytest.fixture
-def requiredMetadataDict():
-    """
-    A sample dictionary that has all required metadata for constructing a
-    BIDS-Incremental
-    """
-    return {'subject': '01', 'task': 'aTask', 'suffix': 'bold'}
-
-@pytest.fixture
-def validBidsI(sampleNiftiImage, requiredMetadataDict):
-    """
-    Constructs and returns a known-valid BIDS-Incremental using known metadata.
-    """
-    return BidsIncremental(image=sampleNiftiImage,
-                           subject=requiredMetadataDict["subject"],
-                           task=requiredMetadataDict["task"],
-                           suffix=requiredMetadataDict["suffix"])
-
+# Permutations that the constructor must validate
 nonePermutations = [{"subject": None, "task": None, "suffix": None},
                     {"subject": "test", "task": None, "suffix": None},
                     {"subject": "test", "task": "test", "suffix": None}]
 
+
 @pytest.mark.parametrize("argDict", nonePermutations)
-def testNullConstruction(sampleNiftiImage, requiredMetadataDict, argDict):
+def testNullConstruction(sampleNiftiImage, imageMetadataDict, argDict):
     # Test empty image
     with pytest.raises(ValidationError):
         BidsIncremental(image=None,
-                        subject=requiredMetadataDict["subject"],
-                        task=requiredMetadataDict["task"],
-                        suffix=requiredMetadataDict["suffix"])
+                        subject=imageMetadataDict["subject"],
+                        task=imageMetadataDict["task"],
+                        suffix=imageMetadataDict["suffix"])
 
         # Test empty required fields
         for p in nonePermutations:
@@ -75,48 +39,50 @@ def testNullConstruction(sampleNiftiImage, requiredMetadataDict, argDict):
                                 suffix=p["suffix"])
             assert "Image" not in str(excinfo.value)
 
-def testValidConstruction(sampleNiftiImage, requiredMetadataDict):
+
+def testValidConstruction(sampleNiftiImage, imageMetadataDict):
     bidsInc = BidsIncremental(image=sampleNiftiImage,
-                              subject=requiredMetadataDict["subject"],
-                              task=requiredMetadataDict["task"],
-                              suffix=requiredMetadataDict["suffix"])
+                              subject=imageMetadataDict["subject"],
+                              task=imageMetadataDict["task"],
+                              suffix=imageMetadataDict["suffix"])
     assert bidsInc is not None
 
+
 # Test that invalid dataset.json fields are rejected and valid ones are accepted
-def testDatasetMetadata(sampleNiftiImage, requiredMetadataDict):
+def testDatasetMetadata(sampleNiftiImage, imageMetadataDict):
     # Test invalid dataset metadata
     with pytest.raises(ValidationError):
         BidsIncremental(image=sampleNiftiImage,
-                        subject=requiredMetadataDict["subject"],
-                        task=requiredMetadataDict["task"],
-                        suffix=requiredMetadataDict["suffix"],
+                        subject=imageMetadataDict["subject"],
+                        task=imageMetadataDict["task"],
+                        suffix=imageMetadataDict["suffix"],
                         datasetMetadata={"random_field": "doesnt work"})
 
     # Test valid dataset metadata
     dataset_name = "Test dataset"
     bidsInc = BidsIncremental(image=sampleNiftiImage,
-                              subject=requiredMetadataDict["subject"],
-                              task=requiredMetadataDict["task"],
-                              suffix=requiredMetadataDict["suffix"],
+                              subject=imageMetadataDict["subject"],
+                              task=imageMetadataDict["task"],
+                              suffix=imageMetadataDict["suffix"],
                               datasetMetadata={"Name": dataset_name,
                                                "BIDSVersion": "1.0"})
     assert bidsInc.datasetName() == dataset_name
 
 
-
 # Test that extracting metadata from the BIDS-I using its provided API returns
 # the correct values
-def testMetadataOutput(validBidsI, requiredMetadataDict):
+def testMetadataOutput(validBidsI, imageMetadataDict):
     with pytest.raises(ValueError):
         validBidsI.getEntity("InvalidEntityName")
 
     # Data type - always 'func' currently
     assert validBidsI.dataType() == "func"
     # Entities
-    assert validBidsI.getEntity('subject') == requiredMetadataDict["subject"]
-    assert validBidsI.getEntity('task') == requiredMetadataDict["task"]
+    assert validBidsI.getEntity('subject') == imageMetadataDict["subject"]
+    assert validBidsI.getEntity('task') == imageMetadataDict["task"]
     # Suffix
-    assert validBidsI.suffix() == requiredMetadataDict["suffix"]
+    assert validBidsI.suffix() == imageMetadataDict["suffix"]
+
 
 # Test that the BIDS-I properly parses BIDS fields present in a DICOM
 # ProtocolName header field
@@ -128,6 +94,7 @@ def testParseProtocolName():
 
     for key, expectedValue in expectedValues.items():
         assert parsedValues[key] == expectedValue
+
 
 # Test that the BIDS-I interface methods for extracting internal NIfTI data
 # return the correct values
@@ -153,29 +120,40 @@ def testQueryNifti(validBidsI):
     FIELD = "dim"
     assert np.array_equal(queriedHeader.get(FIELD), exactHeader.get(FIELD))
 
+
 # Test that constructing BIDS-compatible filenames from internal metadata
 # returns the correct filenames
-def testFilenameConstruction(validBidsI, requiredMetadataDict):
-    baseFilename = "sub-{}_task-{}_{}".format(
-        requiredMetadataDict["subject"],
-        requiredMetadataDict["task"],
-        requiredMetadataDict["suffix"])
+def testFilenameConstruction(validBidsI, imageMetadataDict):
+    """
+    General format:
+    sub-<label>[_ses-<label>]_task-<label>[_acq-<label>] [_ce-<label>]
+        [_dir-<label>][_rec-<label>][_run-<index>]
+        [_echo-<index>]_<contrast_label >.ext
+    """
+    baseFilename = "sub-{sub}_ses-{ses}_task-{task}_run-{run}_{suf}".format(
+        sub=imageMetadataDict["subject"],
+        ses=imageMetadataDict["session"],
+        task=imageMetadataDict["task"],
+        run=imageMetadataDict["run"],
+        suf=imageMetadataDict["suffix"])
 
     assert baseFilename + ".nii" == \
         validBidsI.makeBidsFileName(BidsFileExtension.IMAGE)
     assert baseFilename + ".json" == \
         validBidsI.makeBidsFileName(BidsFileExtension.METADATA)
 
+
 # Test that the hypothetical path for the BIDS-I if it were in an archive is
 # correct based on the metadata within it
-def testArchivePathConstruction(validBidsI, requiredMetadataDict):
+def testArchivePathConstruction(validBidsI, imageMetadataDict):
     session = "01"
     validBidsI.addEntity("session", session)
 
     assert validBidsI.dataDirPath() == \
-        "/sub-{}/ses-{}/func/".format(requiredMetadataDict["subject"], session)
+        "/sub-{}/ses-{}/func/".format(imageMetadataDict["subject"], session)
 
     validBidsI.removeEntity("session")
+
 
 # Test that writing the BIDS-I to disk returns a properly formatted BIDS archive
 # in the correct location with all the data in the BIDS-I
@@ -189,6 +167,7 @@ def testDiskOutput(validBidsI):
 
     # Cleanup temp directory if test succeeded; leave for inspection otherwise
     shutil.rmtree(datasetPath)
+
 
 # Test serialization results in equivalent BIDS-I object
 def testSerialization(validBidsI):
