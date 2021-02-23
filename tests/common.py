744a4835f5ed29d480c2290b5afbc3109976c332
Stephen Polcyn <spolcyn@users.noreply.github.com>
2021-02-23 10:26:53 -0500

    Update tests with new path variables

--- tests/common.py
+++ tests/common.py
@@ -4,7 +4,7 @@
 import os
 
 """ Imaging inputs """
-test_inputDir = 'test_input'
+test_inputDirName = 'test_input'
 test_dicomFile = '001_000013_000005.dcm'
 test_dicomTruncFile = 'trunc_001_000013_000005.dcm'
 test_nifti1_3DFile = 'test_input_3d_func_ses-01_task-story_run-01_bold.nii'
@@ -13,8 +13,9 @@ test_nifti1_4DFile = 'test_input_4d_func_ses-01_task-story_run-01_bold.nii'
 test_nifti2_4DFile = 'test_input_4d_nifti2_func_ses-01_task-story_run-01_bold.nii'
 
 # absolute paths derived from above names
-test_inputDirPath = os.path.join(os.path.dirname(__file__), test_inputDir)
+test_inputDirPath = os.path.join(os.path.dirname(__file__), test_inputDirName)
 test_dicomPath = os.path.join(test_inputDirPath, test_dicomFile)
+test_dicomTruncPath = os.path.join(test_inputDirPath, test_dicomTruncFile)
 test_3DNifti1Path = os.path.join(test_inputDirPath, test_nifti1_3DFile)
 test_3DNifti2Path = os.path.join(test_inputDirPath, test_nifti2_3DFile)
 test_4DNifti1Path = os.path.join(test_inputDirPath, test_nifti1_4DFile)
